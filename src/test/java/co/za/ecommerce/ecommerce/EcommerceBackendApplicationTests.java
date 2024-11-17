345783919ea4ce6f579b56b0ef428990b75a18b2
Thendo Ramashia <kramashia101@gmail.com>
2024-11-17 21:47:11 +0200

    Initial commit.

---
+++ src/test/java/co/za/ecommerce/ecommerce/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.ecommerce.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
