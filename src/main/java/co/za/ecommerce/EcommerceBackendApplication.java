fe723054bbdcf7c499e680124c8258fdf75a7b76
Thendo Ramashia <kramashia101@gmail.com>
2024-11-18 22:08:20 +0200

    Renamed package

--- src/main/java/co/za/ecommerce/EcommerceBackendApplication.java
+++ src/main/java/co/za/ecommerce/EcommerceBackendApplication.java
@@ -1,4 +1,4 @@
-package co.za.ecommerce.ecommerce;
+package co.za.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/co/za/ecommerce/EcommerceBackendApplicationTests.java
+++ src/test/java/co/za/ecommerce/EcommerceBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package co.za.ecommerce.ecommerce;
+package co.za.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
