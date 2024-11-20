76b34a2e8c909b44f8fe8a5e6424d1262c46b381
Thendo Ramashia <kramashia101@gmail.com>
2024-11-20 19:58:15 +0200

    User Entity

---
+++ src/main/java/co/za/ecommerce/model/User.java
@@ -0,0 +1,85 @@
+package co.za.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@Document(collection = "users")
+public class User extends Entity {
+
+    /**
+     * User full name.
+     */
+    @NonNull
+    private String name;
+
+    /**
+     * User email address.
+     */
+    @NonNull
+    private String email;
+
+    /**
+     * User phone number.
+     */
+    @NonNull
+    private String phone;
+
+    /**
+     * User status
+     */
+    private Status status;
+
+    /**
+     * User code.
+     */
+    private String code;
+
+    /**
+     * User password used mostly for Counter users
+     */
+    @JsonIgnore
+    @NonNull
+    private byte[] password;
+
+    @JsonIgnore
+    @NonNull
+    private byte[] salt;
+
+    /**
+     * Counters ID
+     */
+    private Set<String> assignedCounter;
+
+    /**
+     * User roles assigned to
+     */
+    private Set<String> roles;
+
+    public enum Status {
+        ACTIVE,SUSPENDED,AWAITING_CONFIRMATION
+    }
+
+    public void addRoles(String... newRoles) {
+        if (roles == null) roles = new HashSet<>();
+        roles.addAll(Arrays.asList(newRoles));
+    }
+
+    public void removeRoles(String... newRoles) {
+        if (roles == null) return;
+        Arrays.asList(newRoles).forEach(roles::remove);
+    }
+
+}
