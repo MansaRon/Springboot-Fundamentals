8db55b4000706dde2cfad12724a3a3c5222b4dee
Thendo Ramashia <kramashia101@gmail.com>
2025-01-22 21:07:22 +0200

    Removed unused import

--- src/main/java/co/za/ecommerce/repository/CustomProductRepository.java
+++ src/main/java/co/za/ecommerce/repository/CustomProductRepository.java
@@ -3,7 +3,6 @@ package co.za.ecommerce.repository;
 import co.za.ecommerce.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.repository.Query;
 
 public interface CustomProductRepository {
     Page<Product> findByCategoryIgnoreCase(String category, Pageable pageable);
